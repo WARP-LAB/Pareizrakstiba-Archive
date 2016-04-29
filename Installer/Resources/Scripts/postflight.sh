4f9ac37978f6fa785b57913764a42aaa0e69d4c5
kroko <code@kroko.me>
2016-04-29 12:58:14 +0300

    additions to postflght that needs testing

--- Installer/Resources/Scripts/postflight.sh
+++ Installer/Resources/Scripts/postflight.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
+
+# Pareizrakstiba installer postflight
+
+# major.minor.revision
+SYSVER="$(sw_vers -productVersion)"
+# major.minor
+SYSVER_MIN="${SYSVER#*.}"
+# minor
+SYSVER_MIN="${SYSVER_MIN%%.*}"
+
+# Set permissions based on OS version
+if [ $SYSVER_MIN -gt "6" ];
+then
 sudo chown -R root:wheel /Library/Services/
 sudo chmod -R 755 /Library/Services/
-#/System/Library/CoreServices/pbs -flush
+else
+sudo chown -R root:admin /Library/Services/
+sudo chmod -R 775 /Library/Services/
+fi
+
+if [ $SYSVER_MIN -gt "5" ];
+then
+sudo -u $USER defaults write -g NSSpellCheckerAutomaticallyIdentifiesLanguages -bool FALSE
+sudo -u $USER defaults write -g NSPreferredSpellServerLanguage 'lv'
+fi
+
+sudo -u $USER  open -n /Library/Services/Pareizrakstiba.service/
+
 
